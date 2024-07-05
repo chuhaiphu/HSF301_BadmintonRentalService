704cfaf00160bb86c64c97330a6ec99cdd13a574
Doan Vinh Phu <doanvinhphu@Doans-MacBook-Pro.local>
2024-07-05 20:50:57 +0700

    first commit

---
+++ src/main/java/hsf/HSF301_BigAssignment/Hsf301BigAssignmentApplication.java
@@ -0,0 +1,13 @@
+package hsf.HSF301_BigAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hsf301BigAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hsf301BigAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hsf/HSF301_BigAssignment/Hsf301BigAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package hsf.HSF301_BigAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hsf301BigAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
