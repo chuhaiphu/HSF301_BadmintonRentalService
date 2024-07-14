32ada0478293ceabadaff2dfdfb43f566e9307e3
ViggaD <danhkvtse172932@gmail.com>
2024-07-14 16:33:58 +0700

    hot fix payment and cart

--- src/main/java/hsf/HSF301_BigAssignment/controller/PaymentController.java
+++ src/main/java/hsf/HSF301_BigAssignment/controller/PaymentController.java
@@ -7,14 +7,11 @@ import hsf.HSF301_BigAssignment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.*;
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> 77fd3141bc5b02b4c13ccbe23e72aa6e932702ab
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
